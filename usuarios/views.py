5a876596b669bd6af03ca00fe71597b684b3509d
FlavianoRS <83477815+FlavianoRS@users.noreply.github.com>
2023-02-01 21:22:47 -0300

    Criando interface login e moostrar produtos

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirSenha = request.POST.get('confirSenha')
+        
+        return HttpResponse(f'Nome: {nome}\n Email:{email}\n Senha:{senha}')
+    else:
+        return render(request, 'login.html')
